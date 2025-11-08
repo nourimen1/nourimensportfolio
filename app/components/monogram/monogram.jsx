249969fc971353912fcf2a665cc63b01276e7807
nourimen1 <nouriimendarragi@gmail.com>
2025-11-08 21:10:46 +0100

    2nd edit

--- app/components/monogram/monogram.jsx
+++ app/components/monogram/monogram.jsx
@@ -18,7 +18,7 @@ export const Monogram = forwardRef(({ highlight, className, ...props }, ref) => 
     >
       <defs>
         <clipPath id={clipId}>
-          <path d="M0 0h6.5a6 6 0 0 1 5.2 3.1L19.4 17l4-9L19 0h6.5a6 6 0 0 1 5.2 3.1L39.5 19 35 29 24.5 10 16 29 0 0Zm46.7 2.8A2 2 0 0 0 45 0h-7l5.5 10 3.2-7.2Z" />
+          <path d="M0 0h6.5a6 6 0 0 1 5.2 3.1L8 12l-4-9L4 0h6.5a6 6 0 0 1 5.2 3.1L20 19l-4 10-8-9L0 29V0L28 29h6.5a6 6 0 0 1 5.2 3.1L44 19l-4 10-8-9L28 0V29z" />
         </clipPath>
       </defs>
       <rect clipPath={`url(#${clipId})`} width="100%" height="100%" />
