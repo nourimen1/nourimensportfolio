04cf3c2a9a12afe2a67e31c54baa55dbed5581c7
nourimen1 <nouriimendarragi@gmail.com>
2025-11-11 17:00:32 +0100

    trying..

--- netlify/functions/server.js
+++ netlify/functions/server.js
@@ -1,4 +1,4 @@
-import { createRequestHandler } from "@netlify/remix-adapter";
+import { createRequestHandler } from "@netlify/remix-adapter/node";
 import * as build from "../../build/server/index.js";
 
 const handler = createRequestHandler({
